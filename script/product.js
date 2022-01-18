d27d8a311b84d7c1c309641911975ad27babfbbd
valeriyanechy <valeriyanechyporuk@gmail.com>
2022-01-18 13:11:12 +0100

    finished

---
+++ script/product.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
